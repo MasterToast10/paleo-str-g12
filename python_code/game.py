5cde3414662506abd962b37e595bdffcc17c44fe
Vash Patrick Ancheta <vashpatrickancheta@gmail.com>
2020-04-27 18:28:27 +0800

    Truly unbeatable MG

--- python_code/game.py
+++ python_code/game.py
@@ -1,8 +1,8 @@
 from engine import GameController, MoveGenerator
 from rmg import RandomMoveGenerator
-from unbeatable import UnbeatableMoveGenerator
+from robbiebarrat_unbeatable import UnbeatableMoveGenerator
 
-controller = GameController(RandomMoveGenerator(1), UnbeatableMoveGenerator(2))
+controller = GameController(UnbeatableMoveGenerator(1), UnbeatableMoveGenerator(2))
 if (the_winner := controller.start()) < 3:
     print(f"Player {the_winner} won!")
 else:
