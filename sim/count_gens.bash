ff342b70864c235d3f39d3eac187009083cabd1b
Vash Patrick Ancheta <vashpatrickancheta@gmail.com>
2020-05-18 22:53:56 +0800

    Preparation for R

--- sim/count_gens.bash
+++ sim/count_gens.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
+echo "generations, group" > r_prep.csv
+
 for dir in */; do
-    touch ${dir%/}_gencount.csv
-    echo "Simulation, Generations" > ${dir%/}_gencount.csv
     for sim in {1..30}; do
-        echo "$sim", $(ls "$dir"sim_"$sim" | wc -l) >> ${dir%/}_gencount.csv
+        echo "$(ls "$dir"sim_"$sim" | wc -l),${dir%/}" >> r_prep.csv
     done
 done
